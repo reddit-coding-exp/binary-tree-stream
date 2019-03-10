8943f7c9cdc8ad09ddd41863382b5a2542d2f727
Anthony Naddeo <anthony.naddeo+wls-bionic@gmail.com>
2019-03-09 18:51:58 -0800

    Adding solutions for tree traversal

--- src/__tests__/binary-tree-test.ts
+++ src/__tests__/binary-tree-test.ts
@@ -1,32 +1,64 @@
 import {preOrder, inOrder, levelOrder, postOrder, Tree} from '../binary-tree'
 
+const myTree : Tree = {
+    value: 8,
+    left: {
+        value: 5,
+        left: {
+            value: 9
+        },
+        right: {
+            value: 7,
+            left: {
+                value: 1
+            },
+            right: {
+                value: 12,
+                left: {
+                    value: 2
+                }
+            }
+        }
+    },
+    right: {
+        value: 4,
+        right: {
+            value: 11,
+            left: {
+                value: 3
+            }
+        }
+
+    }
+}
+
 
 describe('binary trees', () => {
 
     describe('traversal', () => {
 
         it('preOrder works as expected', () => {
-            const tree : Tree = null as any // TODO implement
+            const tree : Tree = myTree
             const  preOrderResults = preOrder(tree)
             expect(preOrderResults).toEqual([8, 5, 9, 7, 1, 12, 2, 4, 11, 3 ])
         })
 
         it('inOrder works as expected', () => {
-            const tree : Tree = null as any // TODO implement
+            const tree : Tree = myTree
             const  inOrderResults = inOrder(tree)
             expect(inOrderResults).toEqual([9, 5, 1, 7, 2, 12, 8, 4, 3, 11 ])
         })
+
         it('postOrder works as expected', () => {
-            const tree : Tree = null as any // TODO implement
+            const tree : Tree = myTree
             const  postOrderResults = postOrder(tree)
             expect(postOrderResults).toEqual([9, 1, 2, 12, 7, 5, 3, 11, 4, 8 ])
         })
 
         it('levelOrder works as expected', () => {
-            const tree : Tree = null as any // TODO implement
+            const tree : Tree = myTree
             const  levelOrderResults = levelOrder(tree)
             expect(levelOrderResults).toEqual([8, 5, 4, 9, 7, 11, 1, 12, 3, 2])
         })
     })
 })
-
--- src/binary-tree.ts
+++ src/binary-tree.ts
@@ -2,25 +2,70 @@
 // https://www.cs.cmu.edu/~adamchik/15-121/lectures/Trees/trees.html
 
 export interface Tree {
-    // TODO implement
+    value: number
+    left?: Tree
+    right?: Tree
 }
 
-export function preOrder(tree: Tree): number[] {
-    // TODO implement
-    return []
+
+export function preOrder(tree: Tree | undefined): number[] {
+    if(!tree){
+        return []
+    }
+
+    return [
+        tree.value,
+        ...preOrder(tree.left),
+        ...preOrder(tree.right)
+    ]
 }
 
-export function inOrder(tree: Tree): number[] {
-    // TODO implement
-    return []
+
+
+export function inOrder(tree: Tree| undefined): number[] {
+    if(!tree){
+        return []
+    }
+
+    return [
+        ...inOrder(tree.left),
+        tree.value,
+        ...inOrder(tree.right)
+    ]
 }
 
-export function levelOrder(tree: Tree): number[] {
-    // TODO implement
-    return []
+export function levelOrder(tree: Tree ): number[] {
+
+    const queue: Tree[] = [tree]
+    const results: number[] = []
+    while(queue.length > 0){
+        const t = queue.shift()
+
+        if(t){
+            results.push(t.value)
+
+            if(t.left){
+                queue.push(t.left)
+            }
+
+            if(t.right){
+                queue.push(t.right)
+            }
+        }
+    }
+
+    return results
 }
 
-export function postOrder(tree: Tree): number[] {
-    // TODO implement
-    return []
+
+export function postOrder(tree: Tree | undefined): number[] {
+    if(!tree){
+        return []
+    }
+
+    return [
+        ...postOrder(tree.left),
+        ...postOrder(tree.right),
+        tree.value
+    ]
 }
